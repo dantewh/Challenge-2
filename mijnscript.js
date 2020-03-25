c19eece2d332aa3dfdbeea54b41165c688a1b195
dantewh <61277342+dantewh@users.noreply.github.com>
2020-03-25 14:38:55 +0100

    verder met js, gaat nog niet echt lekker
    ook de css aangepast, zat klein foutje in. nu zie je wel de uren wijzer

--- mijnscript.js
+++ mijnscript.js
@@ -4,19 +4,21 @@ const hourDiv = document.getElementById('hour');
 
 setInterval(updateClock, 1000);
 
+
 function updateClock(){
-    Let date = new Date();
-    Let sec = date.getSeconds() / 60;
-    Let min = date.getMinutes() / 60;
-    Let hour = date.getHours() / 12;
+    let date = new Date();
+    let sec = date.getSeconds() / 60;
+    let min = date.getMinutes() / 60;
+    let hour = date.getHours() / 12;
     
     secDiv.style.Transform = "rotate(" + (sec * 360) + "deg)";
     minDiv.style.Transform = "rotate(" + (min * 360) + "deg)";
     hourDiv.style.Transform = "rotate(" + (hour * 360) + "deg)";
 }
 
-function myFunction() {
-  alert("I am an alert box!");
-}
-
-window.onload = myFunction();
+updateClock();
+//function myFunction() {
+//  alert("I am an alert box!");
+//}
+//
+//window.onload = myFunction();
